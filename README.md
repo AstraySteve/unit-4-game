4daa55d6c896b9efb21b01a52e03627d3d1a4ba2
S Arashi <34376591+AstraySteve@users.noreply.github.com>
2018-04-27 22:15:15 -0400

    started frameworking and layout

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # unit-4-game
 jQuery Assignment
+
+Inspired by mobile game "FATE/ GRAND ORDER" by Type-Moon and Delight-Works
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,3 +3,28 @@
     Assignment 4, 2018
     UofT SCS Coding Bootcamp
 */
+
+//Object containing base info for characters
+var Saber = {
+    name: "Saber (Artoria Pendragon)",
+    health: 100, //TODO give health actual values
+    attackPower: 50, //TODO give attackPower actual values
+};
+
+var Archer = {
+    name: "Archer (EMIYA)",
+    health: 100, //TODO give health actual values
+    attackPower: 50, //TODO give attackPower actual values
+};
+
+var Lancer = {
+    name: "Lancer (Cu Chulainn)",
+    health: 100, //TODO give health actual values
+    attackPower: 50, //TODO give attackPower actual values
+};
+
+var Sheilder = {
+    name: "Sheilder (Mashu Kyrielite)",
+    health: 100, //TODO give health actual values
+    attackPower: 50, //TODO give attackPower actual values
+};
