8a6613f62076f53e810425e74dc8ab07adb95712
S Arashi <34376591+AstraySteve@users.noreply.github.com>
2018-04-28 19:10:02 -0400

    new sprites, functionality up to enemy select

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,7 +10,8 @@ var Saber = {
     health: 100, //TODO give health actual values
     attackPower: 50, //TODO give attackPower actual values
 
-    image: "Artoria2.png",
+    imageCard: "Artoria2.png",
+    imageSprite: "Arthur_Sprite.png",
 };
 
 var Archer = {
@@ -18,7 +19,8 @@ var Archer = {
     health: 100, //TODO give health actual values
     attackPower: 50, //TODO give attackPower actual values
 
-    image: "Emiya2.png",
+    imageCard: "Emiya2.png",
+    imageSprite: "Emiya_Sprite.png",
 };
 
 var Lancer = {
@@ -26,7 +28,8 @@ var Lancer = {
     health: 100, //TODO give health actual values
     attackPower: 50, //TODO give attackPower actual values
 
-    image: "Cuchulainn1.png",
+    imageCard: "Cuchulainn1.png",
+    imageSprite: "Cu_Sprite.png",
 };
 
 var Sheilder = {
@@ -34,36 +37,58 @@ var Sheilder = {
     health: 100, //TODO give health actual values
     attackPower: 50, //TODO give attackPower actual values
 
-    image: "Shielder2.png",
+    imageCard: "Shielder2.png",
+    imageSprite: "Mashu_Sprite.png",
 };
 
 //Global Variables
 var mainList = [Saber, Archer, Lancer, Sheilder];
 var player; //variable to hold player's choice
 var enemyList = []; //list to hold remaining enemies
+var enemy; //Variable to hold current enemy
+var enemyFlag = false; //flag if enemy has been selected
 
 //Functions
 function showEnemyList(){
-    //console.log(enemyList.length);
+    //Function dynamically creates buttons for enemy selection
+    $("#enemies").empty(); //clears the div
+
     for (i=0; i<enemyList.length; i++){
-        console.log(enemyList.length);
         var servantCard = $("<button>");
         servantCard.attr({
-            class: "btn btn-warning servant-card col-3 p-0 m-1",
+            class: "btn btn-warning servant-card col-2 p-0 m-1",
             id: "enemy" + i,
             value: i,
         });
-        $("#fightList").append(servantCard);
+        $("#enemies").append(servantCard);
 
+        //Adds <img> under <button>
         var servantImage = $("<img>");
         servantImage.attr({
-            src: "assets/images/" + enemyList[i].image,
+            src: "assets/images/" + enemyList[i].imageCard,
             alt: "SERVANT",
             class: "img-fluid card-border",
         });
-        
         $("#enemy" + i).append(servantImage);
     }
+}
+
+function initPlayer(){
+    //Function to initialize player side
+    $("#playerName").text(player.name);
+    $("#playerHP").text(player.health);
+    $('#playerImg').attr({
+        src: "assets/images/" + player.imageSprite,
+    });
+}
+
+function initEnemy(){
+    //Function to initialize enemy side
+    $("#enemyName").text(enemy.name);
+    $("#enemyHP").text(enemy.health);
+    $('#enemyImg').attr({
+        src: "assets/images/" + enemy.imageSprite,
+    });
 }
 
 $(document).ready(function() {
@@ -80,18 +105,26 @@ $(document).ready(function() {
             }
         }
         showEnemyList();
+        initPlayer();
 
         //Hide character selection block and display main game interface
         $("#selection-block").attr("style", "display: none");
         //$("#main-game").attr("style", "display: block"); //TEMP: re-add for game functionality
-
-        //TODO TEST CODE, CHANGE FOR GAME FUNCTIONALITY
-        //alert("Servant Selected: " + mainList[this.value].name);
-        //DEBUG CODE REMOVE WHEN DONE
-        //console.log(enemyList);
-        //console.log(player.name);
+    });
+    
+    //Enemy Selection Event
+    $("#enemies").on("click", ".servant-card", function(){
+        if (enemyFlag){
+            alert("Finish current battle!");
+        }
+        else{
+            enemyFlag = true; //flag prevent selecting another opponent
+            enemy = enemyList[this.value];
+            enemyList.splice(this.value,1); //remove item from array
+            initEnemy();
+            //console.log(enemyList);
+            showEnemyList();
+        }
     });
 
-    //TEST CODE REMOVE WHEN DONE
-    
 });
